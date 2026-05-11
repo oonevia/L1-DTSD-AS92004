8a9a27134ecc5f521a9d8304ab9e61f2005fdee2
oonevia <russelldamascenol@stu.otc.school.nz>
2026-05-12 10:10:47 +1200

    YESS

--- Quiz.py
+++ Quiz.py
@@ -7,6 +7,7 @@ countq = 0
 counta = 0
 acheck = 0
 responses = []
+errors = []
 questions = ["What player is this", 
              "Which Premier League club did Chris Wood join from Leeds United in 2017?", 
              "At what age did Chris Wood make his debut for the New Zealand senior national team (the All Whites)", 
@@ -28,6 +29,9 @@ root.attributes("-fullscreen", True)
 root.geometry("1000x1000")
 check = tk.IntVar(value=0)
 
+def clear_screen():
+    for widget in root.winfo_children():
+        widget.destroy()
 def available():
     submitbutton.configure(state="normal")
 def submit():
@@ -36,31 +40,42 @@ def submit():
     print(option)
     global callback
     callback = True
+    clear_screen()
+    draw()
     return option
 
-while countqd < 9:
 
-    callback = False
-    countqd += 1
-    countq += 1
-    counta = 0
-    label = tk.CTkLabel(root, text=f"Chris Wood Quiz:\nQuestion {countq}: {questions[countq]}", font=("Roboto Medium", 38))
-    label.pack(padx=20, pady=20)
-    cb = tk.CTkRadioButton(master=root, text=f"{answers[countqd][countq][counta]}", variable=check, value=1, command=available)
-    cb.pack(padx=20, pady=20)
-    counta += 1
-    cb2 = tk.CTkRadioButton(master=root, text=f"{answers[countqd][countq][counta]}", variable=check, value=2, command=available)
-    cb2.pack(padx=20, pady=20)
-    counta += 1
-    cb3 = tk.CTkRadioButton(master=root, text=f"{answers[countqd][countq][counta]}", variable=check, value=3, command=available)
-    cb3.pack(padx=20, pady=20)
-    counta += 1
+try:
+    def draw():
+        global countqd
+        global countq
+        global counta
+        callback = False
+        countqd += 1
+        countq += 1
+        counta = 0
+        label = tk.CTkLabel(root, text=f"Chris Wood Quiz:\nQuestion {countq}: {questions[countq]}", font=("Roboto Medium", 38))
+        label.pack(padx=20, pady=20)
+        cb = tk.CTkRadioButton(master=root, text=f"{answers[countqd][countq][counta]}", variable=check, value=1, command=available)
+        cb.pack(padx=20, pady=20)
+        counta += 1
+        cb2 = tk.CTkRadioButton(master=root, text=f"{answers[countqd][countq][counta]}", variable=check, value=2, command=available)
+        cb2.pack(padx=20, pady=20)
+        counta += 1
+        cb3 = tk.CTkRadioButton(master=root, text=f"{answers[countqd][countq][counta]}", variable=check, value=3, command=available)
+        cb3.pack(padx=20, pady=20)
+        counta += 1
 
-    submitbutton = tk.CTkButton(root, text="Submit Answer", command=submit, state="disabled")
-    submitbutton.pack(pady=30)
+        global submitbutton
+        submitbutton = tk.CTkButton(root, text="Submit Answer", command=submit, state="disabled")
+        submitbutton.pack(pady=30)
 
-    while callback == True:
-        responses.append(str(submit()))
-        print(responses)
-
-    root.mainloop() 
+        while callback == True:
+            responses.append(str(submit()))
+            print(responses)
+    draw()
+    root.mainloop()
+except Exception as e:
+    errors.append(e)
+    for i in range(len(errors)):
+        print(errors[i])
